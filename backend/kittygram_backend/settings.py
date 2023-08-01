951547ed022347ad1b47e26f5052828deda59b48
Vladislav Kazmin <vl.kazmin@ya.ru>
2023-08-01 22:57:59 +0300

    test

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -2,10 +2,13 @@
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY', "secret")
+SECRET_KEY = os.getenv('SECRET_KEY', 'secret')
 
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
