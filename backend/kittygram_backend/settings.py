b4f1aeac3dd8f0cdfb471f6641eec5b49beb3dff
Vladislav Kazmin <vl.kazmin@ya.ru>
2023-08-04 00:15:12 +0300

    Update ALLOWED_HOSTS

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 DEBUG = bool(os.getenv("DEBUG", False))
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", ["127.0.0.1", "localhost"]).split()
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", ("127.0.0.1 localhost")).split()
 
 
 INSTALLED_APPS = [
